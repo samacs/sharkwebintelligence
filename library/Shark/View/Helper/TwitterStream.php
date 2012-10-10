2ab8dc72c6a70bbafda61798f160624a68913b2d
Saul Martinez <saul.martinez05@gmail.com>
2012-10-10 14:41:44 -0700

    library/Shark/Email.php

--- library/Shark/View/Helper/TwitterStream.php
+++ library/Shark/View/Helper/TwitterStream.php
@@ -2,9 +2,31 @@
 // @codingStandardsIgnoreStart
 class Shark_View_Helper_TwitterStream extends Zend_View_Helper_Abstract {
 
-	public function twitterStream($handle, $count = 5) {
+	public function twitterStream($handle, $count = 5, $cache = true) {
+		if ($cache) {
+			$id = 'tweets_' . $handle;
+			$cache = Zend_Registry::get('Zend_Cache');
+
+			$time = $cache->test($id);
+			if ($time && (($time + 3600) < time())) {
+				$tweets = $this->getTweets($handle, $count);
+				$cache->save($tweets, $id);
+			} else if ($time) {
+				$tweets = $cache->load($id);
+			} else {
+				$tweets = $this->getTweets($handle, $count);
+				$cache->save($tweets, $id);
+			}
+			return $tweets;
+		} else {
+			return $this->getTweets($handle, $count);
+		}
+	}
+
+	private function getTweets($handle, $count)
+	{
 		try {
-			$client = new Zend_Http_Client('http://api.twitter.com/statuses/user_timeline.json');
+			$client = new Zend_Http_Client('http://api.twitter.com/1/statuses/user_timeline.json');
 			$client->setParameterGet('screen_name', $handle);
 			$client->setParameterGet('count', $count);
 			$response = $client->request(Zend_Http_Client::GET);
