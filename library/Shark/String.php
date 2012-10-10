ff665d194bb68601d8262775a5d0aa51dad6322a
Saul Martinez <saul.martinez05@gmail.com>
2012-10-09 18:14:02 -0700

    Target blank.

--- library/Shark/String.php
+++ library/Shark/String.php
@@ -4,17 +4,17 @@ class Shark_String {
 
 	public static function linkify($text)
 	{
-		return preg_replace('/(http?:\/\/\S+)/', '<a href="\1">\1</a>', $text);
+		return preg_replace('/(http?:\/\/\S+)/', '<a href="\1" target="_blank">\1</a>', $text);
 	}
 
 	public static function hashtagify($text)
 	{
-		return preg_replace('/(^|\s)#(\w+)/', '\1#<a href="http://search.twitter.com/search?q=%23\2">\2</a>', $text);
+		return preg_replace('/(^|\s)#(\w+)/', '\1#<a href="http://search.twitter.com/search?q=%23\2" target="_blank">\2</a>', $text);
 	}
 
 	public static function userify($text)
 	{
-		return preg_replace('/(^|\s)@(\w+)/', '\1@<a href="http://twitter.com/\2">\2</a>', $text);
+		return preg_replace('/(^|\s)@(\w+)/', '\1@<a href="http://twitter.com/\2" target="_blank">\2</a>', $text);
 	}
 }
 // @codingStandardsIgnoreEnd
