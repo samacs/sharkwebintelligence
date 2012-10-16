c1988d6ae402c0e3eca4e4445d8177f4e290a485
Saul Martinez <saul.martinez05@gmail.com>
2012-10-15 20:50:58 -0700

    Caching local marketing pages.

--- application/modules/default/controllers/LocalmarketingController.php
+++ application/modules/default/controllers/LocalmarketingController.php
@@ -14,15 +14,19 @@ class LocalmarketingController extends Shark_Controller_Action
 		if (null === $page || empty($page)) {
 			return $this->render('index');
 		}
-		$tag = 'page_localmarketing_' . $page;
-		if (APPLICATION_ENV === 'production') {
-			if (($content = $this->cache->load($tag)) === false) {
-				$content = $this->view->render('localmarketing/pages/' . $page . '.phtml');
-				$this->cache->save($content, $tag);
+		try {
+			$tag = 'page_localmarketing_' . str_replace('-', '_', $page);
+			if (APPLICATION_ENV === 'production') {
+				if (($content = $this->cache->load($tag)) === false) {
+					$content = $this->view->render('localmarketing/pages/' . $page . '.phtml');
+					$this->cache->save($content, $tag);
+				}
+				$this->view->content = $content;
+			} else {
+				$this->view->content = $this->view->render('localmarketing/pages/' . $page . '.phtml');
 			}
-			$this->view->content = $content;
-		} else {
-			$this->view->content = $this->view->render('localmarketing/pages/' . $page . '.phtml');
+		} catch (Exception $e) {
+			var_dump($e);
 		}
 	}
 }
