f66791404dd24c462e3c6c29c2b6ecb9d826f2b2
Saul Martinez <saul.martinez05@gmail.com>
2012-10-16 06:50:27 -0700

    Get slides.

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -48,61 +48,46 @@ class IndexController extends Shark_Controller_Action
      */
     public function indexAction()
     {
-        $cacheId = 'frontpage_slides';
-        try {
-            if ($this->cache && ($content = $this->cache->load($cacheId))) {
-                $this->view->slides = $content;
-            } else {
-                if ($this->cache) {
-                    $this->cache->start($cacheId);
-                }
-                $slides = array();
-                $slide = new stdClass();
-                $slide->order = 0;
-                $slide->image = $this->view->baseUrl('/img/slides/websites.jpg');
-                $slide->title = 'Web Hosting';
-                $slide->link = $this->view->url(array('service' => 'hospedaje-web'), 'services');
-                $slides[] = $slide;
-                $slide = new stdClass();
-                $slide->order = 1;
-                $slide->image = $this->view->baseUrl('/img/slides/redes-sociales.jpg');
-                $slide->title = 'First thumbnail label';
-                //$slide->text = 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.';
-                $slide->link = $this->view->url(array('service' => 'desarrollo-web'), 'services');
-                $slides[] = $slide;
-                $slide = new stdClass();
-                $slide->order = 2;
-                $slide->image = $this->view->baseUrl('/img/slides/seo-smm.jpg');
-                $slide->title = 'Second Thumbnail label';
-                //$slide->text = 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.';
-                $slide->link = $this->view->url(array('service' => 'diseno-web'), 'services');
-                $slides[] = $slide;
-                $slide = new stdClass();
-                $slide->order = 3;
-                $slide->image = $this->view->baseUrl('/img/slides/email-marketing.jpg');
-                $slide->title = 'Third Thumbnail label';
-                //$slide->text = 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.';
-                $slide->link = $this->view->url(array('service' => 'redes-sociales'), 'services');
-                $slides[] = $slide;
-                $slide = new stdClass();
-                $slide->order = 4;
-                $slide->image = $this->view->baseUrl('/img/slides/web-analytics.jpg');
-                $slide->title = 'Fourth Thumbnail label';
-                //$slide->text = 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.';
-                $slide->link = $this->view->url(array('service' => 'email-marketing'), 'services');
-                $slides[] = $slide;
-                $this->view->assign(
-                    array(
-                        'slides' => $slides,
-                    )
-                );
-                if ($this->cache) {
-                    $this->cache->save($slides, $cacheId);
-                }
-            }
-        } catch (Exception $e) {
-            var_dump($e);exit;
-        }
+        $slides = array();
+        $slide = new stdClass();
+        $slide->order = 0;
+        $slide->image = $this->view->baseUrl('/img/slides/websites.jpg');
+        $slide->title = 'Web Hosting';
+        $slide->link = $this->view->url(array('service' => 'hospedaje-web'), 'services');
+        $slides[] = $slide;
+        $slide = new stdClass();
+        $slide->order = 1;
+        $slide->image = $this->view->baseUrl('/img/slides/redes-sociales.jpg');
+        $slide->title = 'First thumbnail label';
+        //$slide->text = 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.';
+        $slide->link = $this->view->url(array('service' => 'desarrollo-web'), 'services');
+        $slides[] = $slide;
+        $slide = new stdClass();
+        $slide->order = 2;
+        $slide->image = $this->view->baseUrl('/img/slides/seo-smm.jpg');
+        $slide->title = 'Second Thumbnail label';
+        //$slide->text = 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.';
+        $slide->link = $this->view->url(array('service' => 'diseno-web'), 'services');
+        $slides[] = $slide;
+        $slide = new stdClass();
+        $slide->order = 3;
+        $slide->image = $this->view->baseUrl('/img/slides/email-marketing.jpg');
+        $slide->title = 'Third Thumbnail label';
+        //$slide->text = 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.';
+        $slide->link = $this->view->url(array('service' => 'redes-sociales'), 'services');
+        $slides[] = $slide;
+        $slide = new stdClass();
+        $slide->order = 4;
+        $slide->image = $this->view->baseUrl('/img/slides/web-analytics.jpg');
+        $slide->title = 'Fourth Thumbnail label';
+        //$slide->text = 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.';
+        $slide->link = $this->view->url(array('service' => 'email-marketing'), 'services');
+        $slides[] = $slide;
+        $this->view->assign(
+            array(
+                'slides' => $slides,
+            )
+        );
     }
 
     /**
